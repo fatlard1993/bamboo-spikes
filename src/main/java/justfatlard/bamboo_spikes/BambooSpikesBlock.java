4341b3f1363ad47ac3c95df4246924337eef3a95
fatlard1993 <fatlard1993>
2020-05-14 09:44:48 -0700

    cleanup

--- src/main/java/justfatlard/bamboo_spikes/BambooSpikesBlock.java
+++ src/main/java/justfatlard/bamboo_spikes/BambooSpikesBlock.java
@@ -35,36 +35,34 @@ public class BambooSpikesBlock extends Block implements Waterloggable {
 	protected static final VoxelShape EAST_SHAPE = Block.createCuboidShape(12.0D, 3.0D, 13.0D, 0.0D, 13.0D, 3.0D);
 	protected static final VoxelShape WEST_SHAPE = Block.createCuboidShape(16.0D, 3.0D, 13.0D, 4.0D, 13.0D, 3.0D);
 
-	public BambooSpikesBlock(Settings settings) {
+	public BambooSpikesBlock(Settings settings){
 		super(settings);
 
-		this.setDefaultState((BlockState)((BlockState)((BlockState)this.stateManager.getDefaultState()).with(FACING, Direction.UP).with(WATERLOGGED, false)));
+		this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(FACING, Direction.UP).with(WATERLOGGED, false));
 	}
 
-	public FluidState getFluidState(BlockState state) {
+	public FluidState getFluidState(BlockState state){
 		return (Boolean)state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
 	}
 
-	public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos) {
-		if ((Boolean)state.get(WATERLOGGED)) {
-			world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
-		}
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos){
+		if((Boolean)state.get(WATERLOGGED)) world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
 
 		return super.getStateForNeighborUpdate(state, facing, neighborState, world, pos, neighborPos);
 	}
 
 	@Override
-	public BlockState getPlacementState(ItemPlacementContext context) {
+	public BlockState getPlacementState(ItemPlacementContext context){
 		BlockPos blockPos = context.getBlockPos();
-		FluidState fluidState = context.getWorld().getFluidState(blockPos);
 		Direction direction = context.getSide();
-		BlockState blockState = context.getWorld().getBlockState(context.getBlockPos().offset(direction.getOpposite()));
+		BlockState blockState = context.getWorld().getBlockState(blockPos.offset(direction.getOpposite()));
+		Boolean waterlogged = context.getWorld().getFluidState(blockPos).getFluid() == Fluids.WATER;
 
-		return (BlockState)((BlockState)this.getDefaultState().with(FACING, blockState.getBlock() == this && blockState.get(FACING) == direction ? direction.getOpposite() : direction)).with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+		return (BlockState)((BlockState)this.getDefaultState().with(FACING, blockState.getBlock() == this && blockState.get(FACING) == direction ? direction.getOpposite() : direction)).with(WATERLOGGED, waterlogged);
 	}
 
 	@Override
-	public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPosition, EntityContext entityContext) {
+	public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPosition, EntityContext entityContext){
 		switch((Direction)blockState.get(FACING)){
 			case DOWN:
 				return DOWN_SHAPE;
@@ -88,7 +86,7 @@ public class BambooSpikesBlock extends Block implements Waterloggable {
 
 	@Override
 	public void onEntityCollision(BlockState blockState, World world, BlockPos blockPosition, Entity entity){
-		if(entity.getType() == EntityType.PLAYER|| entity.getType().getCategory() != EntityCategory.MISC) entity.damage(DamageSource.CACTUS, 2.0F);
+		if(entity.getType() == EntityType.PLAYER || entity.getType().getCategory() != EntityCategory.MISC) entity.damage(DamageSource.CACTUS, 2.0F);
 
 		super.onEntityCollision(blockState, world, blockPosition, entity);
 	}
@@ -96,9 +94,11 @@ public class BambooSpikesBlock extends Block implements Waterloggable {
 	@Override
 	public void onLandedUpon(World world, BlockPos pos, Entity entity, float distance){
 		entity.handleFallDamage(distance, 5.0F);
+
+		super.onLandedUpon(world, pos, entity, distance);
 	}
 
-	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder){
 		builder.add(FACING, WATERLOGGED);
 	}
 
