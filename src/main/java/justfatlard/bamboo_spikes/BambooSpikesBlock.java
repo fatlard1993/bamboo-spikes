29c11fd4911f6834489f7dc6fa03111f271f6c1d
justfatlard <fatlard1993@gmail.com>
2026-01-01 16:30:09 -0800

    update for 1.21.11

--- src/main/java/justfatlard/bamboo_spikes/BambooSpikesBlock.java
+++ src/main/java/justfatlard/bamboo_spikes/BambooSpikesBlock.java
@@ -3,106 +3,114 @@ package justfatlard.bamboo_spikes;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.FacingBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.block.Waterloggable;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityCategory;
-import net.minecraft.entity.EntityContext;
+import net.minecraft.entity.EntityCollisionHandler;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.IWorld;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldView;
+import net.minecraft.world.tick.ScheduledTickView;
 
 public class BambooSpikesBlock extends Block implements Waterloggable {
-	private static final DirectionProperty FACING = FacingBlock.FACING;
-	public static final BooleanProperty WATERLOGGED;
+	private static final EnumProperty<Direction> FACING = FacingBlock.FACING;
+	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
-	protected static final VoxelShape UP_SHAPE = Block.createCuboidShape(13.0D, 12.0D, 13.0D, 3.0D, 0.0D, 3.0D);
-	protected static final VoxelShape DOWN_SHAPE = Block.createCuboidShape(13.0D, 16.0D, 13.0D, 3.0D, 4.0D, 3.0D);
-	protected static final VoxelShape NORTH_SHAPE = Block.createCuboidShape(3.0D, 13.0D, 16.0D, 13.0D, 3.0D, 4.0D);
-	protected static final VoxelShape SOUTH_SHAPE = Block.createCuboidShape(13.0D, 13.0D, 0.0D, 3.0D, 3.0D, 12.0D);
-	protected static final VoxelShape EAST_SHAPE = Block.createCuboidShape(12.0D, 3.0D, 13.0D, 0.0D, 13.0D, 3.0D);
-	protected static final VoxelShape WEST_SHAPE = Block.createCuboidShape(16.0D, 3.0D, 13.0D, 4.0D, 13.0D, 3.0D);
+	protected static final VoxelShape UP_SHAPE = Block.createCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 12.0D, 13.0D);
+	protected static final VoxelShape DOWN_SHAPE = Block.createCuboidShape(3.0D, 4.0D, 3.0D, 13.0D, 16.0D, 13.0D);
+	protected static final VoxelShape NORTH_SHAPE = Block.createCuboidShape(3.0D, 3.0D, 4.0D, 13.0D, 13.0D, 16.0D);
+	protected static final VoxelShape SOUTH_SHAPE = Block.createCuboidShape(3.0D, 3.0D, 0.0D, 13.0D, 13.0D, 12.0D);
+	protected static final VoxelShape EAST_SHAPE = Block.createCuboidShape(0.0D, 3.0D, 3.0D, 12.0D, 13.0D, 13.0D);
+	protected static final VoxelShape WEST_SHAPE = Block.createCuboidShape(4.0D, 3.0D, 3.0D, 16.0D, 13.0D, 13.0D);
 
-	public BambooSpikesBlock(Settings settings){
+	public BambooSpikesBlock(Settings settings) {
 		super(settings);
-
-		this.setDefaultState((BlockState)((BlockState)this.stateManager.getDefaultState()).with(FACING, Direction.UP).with(WATERLOGGED, false));
-	}
-
-	public FluidState getFluidState(BlockState state){
-		return (Boolean)state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
-	}
-
-	public BlockState getStateForNeighborUpdate(BlockState state, Direction facing, BlockState neighborState, IWorld world, BlockPos pos, BlockPos neighborPos){
-		if((Boolean)state.get(WATERLOGGED)) world.getFluidTickScheduler().schedule(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
-
-		return super.getStateForNeighborUpdate(state, facing, neighborState, world, pos, neighborPos);
+		this.setDefaultState(this.stateManager.getDefaultState()
+			.with(FACING, Direction.UP)
+			.with(WATERLOGGED, false));
 	}
 
 	@Override
-	public BlockState getPlacementState(ItemPlacementContext context){
+	public FluidState getFluidState(BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+	}
+
+	@Override
+	protected BlockState getStateForNeighborUpdate(BlockState state, WorldView world, ScheduledTickView tickView, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, Random random) {
+		if (state.get(WATERLOGGED)) {
+			tickView.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+		}
+		return super.getStateForNeighborUpdate(state, world, tickView, pos, direction, neighborPos, neighborState, random);
+	}
+
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext context) {
 		BlockPos blockPos = context.getBlockPos();
 		Direction direction = context.getSide();
 		BlockState blockState = context.getWorld().getBlockState(blockPos.offset(direction.getOpposite()));
-		Boolean waterlogged = context.getWorld().getFluidState(blockPos).getFluid() == Fluids.WATER;
+		boolean waterlogged = context.getWorld().getFluidState(blockPos).getFluid() == Fluids.WATER;
 
-		return (BlockState)((BlockState)this.getDefaultState().with(FACING, blockState.getBlock() == this && blockState.get(FACING) == direction ? direction.getOpposite() : direction)).with(WATERLOGGED, waterlogged);
+		Direction facing = blockState.getBlock() == this && blockState.get(FACING) == direction
+			? direction.getOpposite()
+			: direction;
+
+		return this.getDefaultState()
+			.with(FACING, facing)
+			.with(WATERLOGGED, waterlogged);
 	}
 
 	@Override
-	public VoxelShape getOutlineShape(BlockState blockState, BlockView blockView, BlockPos blockPosition, EntityContext entityContext){
-		switch((Direction)blockState.get(FACING)){
-			case DOWN:
-				return DOWN_SHAPE;
-			case NORTH:
-				return NORTH_SHAPE;
-			case SOUTH:
-				return SOUTH_SHAPE;
-			case WEST:
-				return WEST_SHAPE;
-			case EAST:
-				return EAST_SHAPE;
-			default:
-				return UP_SHAPE;
+	protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		return switch (state.get(FACING)) {
+			case DOWN -> DOWN_SHAPE;
+			case NORTH -> NORTH_SHAPE;
+			case SOUTH -> SOUTH_SHAPE;
+			case WEST -> WEST_SHAPE;
+			case EAST -> EAST_SHAPE;
+			default -> UP_SHAPE;
+		};
+	}
+
+	@Override
+	protected VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		// Return empty collision so entities can walk into the spikes and trigger onEntityCollision
+		return VoxelShapes.empty();
+	}
+
+	@Override
+	protected void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler, boolean collidedWithFluid) {
+		if (!world.isClient() && world instanceof ServerWorld serverWorld) {
+			if (entity.getType() == EntityType.PLAYER || entity.getType().getSpawnGroup() != SpawnGroup.MISC) {
+				entity.damage(serverWorld, world.getDamageSources().cactus(), 2.0F);
+			}
 		}
 	}
 
 	@Override
-	public VoxelShape getCollisionShape(BlockState blockState, BlockView blockView, BlockPos blockPosition, EntityContext entityContext){
-		return this.collidable ? blockState.getOutlineShape(blockView, blockPosition) : VoxelShapes.empty();
+	public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, double fallDistance) {
+		if (!world.isClient() && world instanceof ServerWorld serverWorld) {
+			entity.handleFallDamage((float) fallDistance, 5.0F, serverWorld.getDamageSources().fall());
+		}
+		super.onLandedUpon(world, state, pos, entity, fallDistance);
 	}
 
 	@Override
-	public void onEntityCollision(BlockState blockState, World world, BlockPos blockPosition, Entity entity){
-		if(entity.getType() == EntityType.PLAYER || entity.getType().getCategory() != EntityCategory.MISC) entity.damage(DamageSource.CACTUS, 2.0F);
-
-		super.onEntityCollision(blockState, world, blockPosition, entity);
-	}
-
-	@Override
-	public void onLandedUpon(World world, BlockPos pos, Entity entity, float distance){
-		entity.handleFallDamage(distance, 5.0F);
-
-		super.onLandedUpon(world, pos, entity, distance);
-	}
-
-	protected void appendProperties(StateManager.Builder<Block, BlockState> builder){
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
 		builder.add(FACING, WATERLOGGED);
-	}
-
-	static {
-		WATERLOGGED = Properties.WATERLOGGED;
 	}
 }
--- src/main/java/justfatlard/bamboo_spikes/Main.java
+++ src/main/java/justfatlard/bamboo_spikes/Main.java
@@ -1,21 +1,49 @@
 package justfatlard.bamboo_spikes;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.block.FabricBlockSettings;
-import net.minecraft.block.Material;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class Main implements ModInitializer {
-	public static final BambooSpikesBlock BAMBOO_SPIKES_BLOCK = new BambooSpikesBlock(FabricBlockSettings.of(Material.BAMBOO).sounds(BlockSoundGroup.BAMBOO).strength(0.6F, 16.0F).build());
+	public static final String MOD_ID = "bamboo-spikes-justfatlard";
+
+	public static final Identifier BAMBOO_SPIKES_ID = Identifier.of(MOD_ID, "bamboo_spikes");
+
+	public static final RegistryKey<net.minecraft.block.Block> BAMBOO_SPIKES_BLOCK_KEY = RegistryKey.of(RegistryKeys.BLOCK, BAMBOO_SPIKES_ID);
+	public static final RegistryKey<Item> BAMBOO_SPIKES_ITEM_KEY = RegistryKey.of(RegistryKeys.ITEM, BAMBOO_SPIKES_ID);
+
+	public static final BambooSpikesBlock BAMBOO_SPIKES_BLOCK = new BambooSpikesBlock(
+		AbstractBlock.Settings.create()
+			.sounds(BlockSoundGroup.BAMBOO)
+			.strength(0.6F, 16.0F)
+			.registryKey(BAMBOO_SPIKES_BLOCK_KEY)
+	);
+
+	public static final BlockItem BAMBOO_SPIKES_ITEM = new BlockItem(
+		BAMBOO_SPIKES_BLOCK,
+		new Item.Settings().registryKey(BAMBOO_SPIKES_ITEM_KEY).useBlockPrefixedTranslationKey()
+	);
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registry.BLOCK, new Identifier("bamboo-spikes-justfatlard", "bamboo_spikes"), BAMBOO_SPIKES_BLOCK);
-		Registry.register(Registry.ITEM, new Identifier("bamboo-spikes-justfatlard", "bamboo_spikes"), new BlockItem(BAMBOO_SPIKES_BLOCK, new Item.Settings().group(ItemGroup.MISC)));
+		Registry.register(Registries.BLOCK, BAMBOO_SPIKES_ID, BAMBOO_SPIKES_BLOCK);
+		Registry.register(Registries.ITEM, BAMBOO_SPIKES_ID, BAMBOO_SPIKES_ITEM);
+
+		// Add to creative inventory tabs
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
+			entries.add(BAMBOO_SPIKES_ITEM);
+		});
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(entries -> {
+			entries.add(BAMBOO_SPIKES_ITEM);
+		});
 	}
 }
